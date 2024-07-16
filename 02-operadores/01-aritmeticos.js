1f588481da0ce2826d37cc0f03a4c01f7f4d46c1
haundrew <andersoon201316@gmail.com>
2024-07-16 15:54:28 -0600

    Nuevos archivos

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 7;
+
+// console.log(a + b, 'suma'); // suma
+// console.log(a - b, 'resta'); // resta
+// console.log(a * b, 'multi'); // multi
+// console.log(a / b, 'division'); // división
+// console.log(a % b, 'modulo'); // madulo
+// console.log(a ** b, 'potencia'); // potencia
+
+//incrementar
+console.log(++a);
+console.log(a++);
+console.log(a);
+//decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);
