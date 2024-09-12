3ecdbdd9590f8b7b241b15645bd6c42fcb29261b
haundrew <andersoon201316@gmail.com>
2024-09-12 15:54:49 -0600

    1.0.1

---
+++ 05-objetos/01-intro.js
@@ -0,0 +1,19 @@
+let email = 'andres@gmail.com';
+let name = 'Andres';
+let direccion = {
+    calle: 'pasaje g',
+    numero: 731,
+};
+
+let user = {
+    email: 'andres@gmail.com',
+    name: 'Andres';
+    direccion: {
+        calle: 'pasaje g',
+        numero: 731,
+    },
+    activo: true,
+    recuperarClave: function () {
+        console.log('Recuperando clave...');
+    },
+};
